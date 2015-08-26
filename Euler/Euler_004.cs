8b3b6dd7e1c42798097673cd4eafd5b0d9f9316b
colvinm0037 <colvinm0037@my.uwstout.edu>
2015-08-26 15:04:50 -0500

    Initial.

---
+++ Euler/Euler_004.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Euler_004
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+
+            for (int i = 999; i > 0; i--)
+            {
+                for (int k = 999; k > 0; k--)
+                {
+                    int number = i * k;
+                    if (isPalindrome(number))
+                    {
+                        if (number > max)
+                            max = number;
+                    }
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+
+
+        static bool isPalindrome(int number)
+        {
+            String s = number.ToString();
+            Stack stack = new Stack();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                stack.Push(s[i]);
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char ch = (char)stack.Pop();
+                if (s[i] != ch)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Euler/Euler_005.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Euler_005
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 20; i < 10000000000; i = i + 20)
+            {
+                if (checkDivisors(i))
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool checkDivisors(int number)
+        {
+            for (int k = 1; k <= 20; k++)
+            {
+                if (number % k != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
