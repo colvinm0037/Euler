eec1c4f4514875fb652aed5b17cb433c4739667a
Micah <colvinm0037@my.uwstout.edu>
2015-10-02 08:34:04 -0500

    Added test project with unit tests for first five problems. Created Answers file to hold answers strings for testing.

---
+++ Euler/Euler/Answers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Euler
+{
+   public class Answers
+   {
+        public static string Probelm_001 = "233168";
+        public static string Probelm_002 = "4613732";
+        public static string Probelm_003 = "6857";
+        public static string Probelm_004 = "906609";
+        public static string Probelm_005 = "232792560";
+
+       
+
+    }
+}
---
+++ EulerTests/Euler/Problems/Euler_Tests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Euler.Euler;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class Euler_Tests
+    {
+        [TestMethod()]
+        public void Problem_001_Test()
+        {
+            Euler_001 euler = new Euler_001();
+            string expected = Answers.Probelm_001;
+            string result = euler.Run();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void Problem_002_Test()
+        {
+            Euler_002 euler = new Euler_002();
+            string expected = Answers.Probelm_002;
+            string result = euler.Run();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void Problem_003_Test()
+        {
+            Euler_003 euler = new Euler_003();
+            string expected = Answers.Probelm_003;
+            string result = euler.Run();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void Problem_004_Test()
+        {
+            Euler_004 euler = new Euler_004();
+            string expected = Answers.Probelm_004;
+            string result = euler.Run();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void Problem_005_Test()
+        {
+            Euler_005 euler = new Euler_005();
+            string expected = Answers.Probelm_005;
+            string result = euler.Run();
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
